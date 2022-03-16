c5dffe5bddfd87a587cfa7bca3680bd197b6fc21
kcngdominic <kcngdominic@gmail.com>
2022-03-16 15:47:58 +1030

    init commit

---
+++ helloworld.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace HelloWorld
+{
+    class Program
+    {
+        // static void Main(string[] args)
+        // {
+        //     foreach (string line in System.IO.File.ReadLines(@".\test.txt"))
+        //     {
+        //         Console.WriteLine(line);
+        //     }
+        //     Console.WriteLine("Hello World!");
+        //     Console.WriteLine("Bye World!");
+        // }
+    }
+}
---
+++ main.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace RdtMarsRover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string line in System.IO.File.ReadLines(@".\test.txt"))
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
